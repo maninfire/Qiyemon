2312a989baf8f1a6261d8d7d899dc886d4954246
张振国 <zhangzhenguo@360.net>
2018-06-28 14:12:11 +0800

    h

--- app/src/main/java/com/qiye/txz/qiyemon/InstrumentationManager.java
+++ app/src/main/java/com/qiye/txz/qiyemon/InstrumentationManager.java
@@ -31,6 +31,7 @@ public class InstrumentationManager implements IXposedHookLoadPackage, IXposedHo
     public boolean off = false;
 
     public static String CONFIG_FILE = "/data/local/tmp/hooks.json";
+    public static String CONFIG_NATIVE_FILE = "/data/local/tmp/hooknatives.json";
     public static Boolean TRACE = false;
 
     private static Set<String> OS_APPS = new HashSet<String>(Arrays.asList(
@@ -90,25 +91,24 @@ public class InstrumentationManager implements IXposedHookLoadPackage, IXposedHo
         if (off == false)
         {
             flushConfig();
-
         }
         if (!loadPackageParam.packageName.equals(GlobalConfig.Inject_PackageName))
             return;
-        Class clazz = XposedHelpers.findClass(GlobalConfig.Inject_ClassName, loadPackageParam.classLoader); //使用注入包的类加载器来反射查找System.load函数,从而调用加载自己的框架so
-        if(clazz == null)
+        Class findloadclazz = XposedHelpers.findClass(GlobalConfig.Inject_ClassName, loadPackageParam.classLoader); //使用注入包的类加载器来反射查找System.load函数,从而调用加载自己的框架so
+        if(findloadclazz == null)
         {
             XposedBridge.log("[-] not found class: " + GlobalConfig.Inject_ClassName);
             Log.e(GlobalConfig.Log_TAG, "[-] can't found class: " + GlobalConfig.Inject_ClassName);
             return;
         }
         Log.e(GlobalConfig.Log_TAG, "[+] found class: " + GlobalConfig.Inject_ClassName);
-        Method method = XposedHelpers.findMethodExact(clazz, GlobalConfig.Inject_Invoke_FunName, String.class);//查找system.load函数
-        if (method == null)
+        Method methodload = XposedHelpers.findMethodExact(findloadclazz, GlobalConfig.Inject_Invoke_FunName, String.class);//查找system.load函数
+        if (methodload == null)
         {
-            Log.e(GlobalConfig.Log_TAG, "[-] can't found function: " + method.getName());
+            Log.e(GlobalConfig.Log_TAG, "[-] can't found function: " + methodload.getName());
         }
-        Log.e(GlobalConfig.Log_TAG, "[+] found function: " + method.getName());
-        method.invoke(null, GlobalConfig.Inject_SoPath);//加载待注入库
+        Log.e(GlobalConfig.Log_TAG, "[+] found function: " + methodload.getName());
+        methodload.invoke(null, GlobalConfig.Inject_SoPath);//加载待注入库
 
     }
 
@@ -130,6 +130,27 @@ public class InstrumentationManager implements IXposedHookLoadPackage, IXposedHo
         return true;
 
     }
+    public void instrumentAppnative() throws FileNotFoundException, IOException
+    {
+        Gson gson = new Gson();
+        String json = Files.readFile(CONFIG_NATIVE_FILE);
+        InstrumentationConfiguration instrumentationConfiguration = gson.fromJson(json, InstrumentationConfiguration.class);
+        TRACE=instrumentationConfiguration.trace;
+        for (HooknativeConfig hooknativeConfig : instrumentationConfiguration.hooknativeConfigs) {
+            //native层的hook不仅仅是传递个so名称和方法名称，还要有适配的参数，这里不是那么容易添加统一的入口。
+            //现有的框架是在interface实现hook,没法传递参数，比较头疼。集团那个沙箱native居然也是用的配置文件，不知道是如何
+            //解决适配问题的。
+            //一般来说只要在每个包里加载一下我们的frame（也就是调用我们的nativeloadhook函数）
+            // ,那么我们的沙箱就可以实现native层所有的函数挂钩
+            //但统一的配置文件挂钩函数有一点小问题的。
+        }
+    }
+    public class HooknativeConfig {
+        private String so_name;
+        private String method;
+        private Boolean thisObject;
+        private String Function;
+    }
 
     public void instrumentApp() throws FileNotFoundException, IOException
     {
@@ -144,6 +165,7 @@ public class InstrumentationManager implements IXposedHookLoadPackage, IXposedHo
 
     public class InstrumentationConfiguration {
         public List<HookConfig> hookConfigs;
+        public List<HooknativeConfig> hooknativeConfigs;
         public Boolean trace;
     }
 
